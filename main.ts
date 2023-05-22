f159fad5019da8640ec41938418a80c6133311b2
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-05-22 10:48:44 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,55 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.showLeds(`
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    . . . . .
+    `)
+radio.setGroup(25)
